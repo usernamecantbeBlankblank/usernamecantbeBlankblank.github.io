3d78934f6a3aeb9fbcbedf0cfa24cac3a3e9b40e
usernamecantbeBlankblank <57266745+usernamecantbeBlankblank@users.noreply.github.com>
2024-07-14 09:52:23 +0200

    audio kapitel 2 und 3 und auch schon umbenannt ;)

--- Assets/voices/mp3UmBennennen.py
+++ Assets/voices/mp3UmBennennen.py
@@ -3,9 +3,10 @@ import os
 def rename_mp3_files(directory):
     files = [f for f in os.listdir(directory) if f.endswith('.mp3')]
     for index, file in enumerate(files):
-        new_name = f"f{index + 1}.mp3"
+        new_name = f"p{index + 1}.mp3"
         os.rename(os.path.join(directory, file), os.path.join(directory, new_name))
     print(f"{len(files)} files renamed.")
 
-directory = r'c:\Users\bianc\OneDrive\Dokumente\GitHub\usernamecantbeBlankblank.github.io\Assets\voices\fridolin'
+directory = r'c:\Users\bianc\OneDrive\Dokumente\GitHub\usernamecantbeBlankblank.github.io\Assets\voices\kapitel3\poco'
 rename_mp3_files(directory)
+
